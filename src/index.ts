96deb0b852c2a7039d83692aff22d46aaf8b0b22
Matheus Graciki <matheusgraciki7@gmail.com>
2023-11-28 07:30:21 -0300

    refactor: consolidate routes into a single file

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
 import express, { Request, Response } from 'express';
-import { UserController } from './controllers/UserController';
+import { router } from './routes';
 
-const userController = new UserController();
 const server = express();
 
 server.use(express.json());
+server.use(router)
 
 server.get('/', (req: Request, res: Response) => {
     return res.status(200).json({ message: 'Did you mean nuddles?' });
 });
 
-server.post('/user', userController.createUser);
-server.get('/user', userController.getAllUsers);
 server.listen(5000, () => console.log('Server on'));
