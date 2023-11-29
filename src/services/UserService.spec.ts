84277b0ee53a94f1709419d72fd24cf94776ce59
Matheus Graciki <matheusgraciki7@gmail.com>
2023-11-29 20:23:11 -0300

    refactor(userservice): implement mockDB
    implement "Dependency Injection" with the mockDB to not use the "real DB"

--- src/services/UserService.spec.ts
+++ src/services/UserService.spec.ts
@@ -1,11 +1,12 @@
-import { UserService } from "./UserService";
+import { User, UserService } from "./UserService";
 
 describe('UserService', () => {
-    const userService = new UserService();
+    const mockDb:User[] = []
+    const userService = new UserService(mockDb);
 
     it('should create a new user', () => {
         const mockConsole = jest.spyOn(global.console, 'log')
         userService.createUser('testname', 'test@test.com')
-        expect(mockConsole).toHaveBeenCalled()
+        expect(mockConsole).toHaveBeenCalledWith( mockDb)
     })
 })
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,3 +1,7 @@
+export interface User {
+    name: string;
+    email: string;
+}
 const db = [
     {
         name: 'Joana',
@@ -9,17 +13,22 @@ const db = [
     },
 ];
 export class UserService {
+    db: User[];
+
+    constructor(database = db) {
+        this.db = database;
+    }
     createUser = (name: string, email: string) => {
         const user = {
             name,
             email,
         };
 
-        db.push(user);
-        console.log(db);
+        this.db.push(user);
+        console.log(this.db);
     };
 
     getAllUsers = () => {
-        return db;
+        return this.db;
     };
 }
