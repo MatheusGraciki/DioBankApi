ab6793978a34c2a6e7700e3c3aa30e3dd0a223a9
Matheus Graciki <matheusgraciki7@gmail.com>
2023-12-04 06:11:30 -0300

    feat(user): test error response on missing user name
    Implemented a test case to verify the correctness of the error response when the user fails to
    provide a name during user creation. The test ensures that the controller correctly returns a 400
    Bad Request status and the expected error message in the response JSON, enhancing the reliability of
    the application in handling missing name scenarios.

--- src/controllers/UserController.spec.ts
+++ src/controllers/UserController.spec.ts
@@ -5,7 +5,7 @@ import { makeMockResponse } from '../__mocks__/mockResponse.mock';
 
 describe('UserController', () => {
     const mockUserService: Partial<UserService> = {
-        createUser: jest.fn()
+        createUser: jest.fn(),
     };
     const userController = new UserController(mockUserService as UserService);
 
@@ -19,6 +19,18 @@ describe('UserController', () => {
         const mockResponse = makeMockResponse();
         userController.createUser(mockRequest, mockResponse);
         expect(mockResponse.state.status).toBe(201);
-        expect(mockResponse.state.json).toMatchObject({ message: 'Account created'})
+        expect(mockResponse.state.json).toMatchObject({ message: 'Account created' });
+    });
+
+    it('should return an error if the user did not enter the name', () => {
+        const mockRequest = {
+            body: {
+                email: 'test@example.com',
+            },
+        } as Request;
+        const mockResponse = makeMockResponse();
+        userController.createUser(mockRequest, mockResponse);
+        expect(mockResponse.state.status).toBe(400);
+        expect(mockResponse.state.json).toMatchObject({ message: 'Bad request! Nome obrigatório' });
     });
 });
